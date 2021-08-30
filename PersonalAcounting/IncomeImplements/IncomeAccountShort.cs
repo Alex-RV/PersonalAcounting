9de569a96dd55290a14affe80dd2441088d6f43a
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2021-08-31 00:04:40 +0300

    [*] что-то

--- PersonalAcounting/IncomeImplements/IncomeAccountShort.cs
+++ PersonalAcounting/IncomeImplements/IncomeAccountShort.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿   using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
@@ -7,24 +7,38 @@ namespace Income
 {
     public class IncomeAccountShort
     {
+        private int _dateTime;
+        private IIncomeList _item;
         private ObservableCollection<IncomeList> _items = new ObservableCollection<IncomeList>();
+        private ObservableCollection<IIncomeList> _itemss = new ObservableCollection<IIncomeList>();
+        private IEnumerable<IIncomeList> _incomeList;
 
         public IncomeAccountShort(IIncomeList item)
         {
-            Item = item;
+            _item = item;
         }
 
         public ObservableCollection<IncomeList> Items { get { return _items; } }
+        public ObservableCollection<IIncomeList> Itemss { get { return _itemss; } }
+        public IEnumerable<IIncomeList> incomeLists { get { return _incomeList; } }
 
-        public IIncomeList Item { get; }
+        //public IIncomeList Item { get; }
 
         public void MonthIncome()
         {
-            foreach (IIncomeList item in Items)
+            _dateTime = DateTime.Now.Day;
+            for (int i = 0; i < _dateTime; i++)
             {
-                
+                foreach (IIncomeList item in Items)
+                {
+                    if (item.Items == _items)
+                    {
 
+                    }
+                    Console.Write(i);
+                }
             }
+           
         }
     }
 }
